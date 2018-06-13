cb0fb84d7ba8bb1c9a1e74a15d6bae4ff906002b
Jake Stanger <jakestanger@jakeMBP-2.local>
2018-06-13 06:44:11 -0600

    body-parser

---
+++ library/src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:authRoutes');
+
+const authRouter = express.Router();
+
+function router() {
+  authRouter.route('/signUp')
+    .post((req, res) => {
+
+    });
+}
+
+module.exports = router;
