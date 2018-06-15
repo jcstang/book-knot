3973bc33d8d23fd9f21ef03d89904b8cefc53ba6
Jake Stanger <jakestanger@jakeMBP-2.local>
2018-06-15 07:20:50 -0600

    added my book to collection

--- library/src/routes/authRoutes.js
+++ library/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
